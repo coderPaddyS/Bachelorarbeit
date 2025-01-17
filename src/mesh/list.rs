6c9a0f49a54c561a09e123bf9738ac7aba89e0a4
Patrick Schneider <urulu@student.kit.edu>
2025-01-17 20:26:36 +0100

    [refactor] removes clone bound on list. [feat] allows to index by other type.

--- src/mesh/list.rs
+++ src/mesh/list.rs
@@ -1,75 +1,54 @@
 use std::{marker::PhantomData, ops::{Deref, DerefMut}, usize};
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct Index<T>
-where
-    T: Clone
-{
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Index<T> {
     index: usize,
     _data: PhantomData<T>
 }
-impl<T> std::fmt::Display for Index<T>
-where
-    T: Clone
-{
+impl<T> std::fmt::Display for Index<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.index)
     }
 }
-impl<T> Deref for Index<T> 
-where
-    T: Clone
-{
+impl<T> Deref for Index<T> {
     type Target = usize;
     fn deref(&self) -> &Self::Target {
         &self.index
     }
 }
-impl<T> DerefMut for Index<T> 
-where
-    T: Clone
-{
+impl<T> DerefMut for Index<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.index
     }
 }
-impl<T> From<usize> for Index<T>
-where
-    T: Clone
-{
+impl<T> From<usize> for Index<T> {
     fn from(value: usize) -> Self {
         Self { index: value, _data: PhantomData::default() }
     }
 }
 
-impl<T> From<Index<T>> for usize
-where
-    T: Clone
-{
+impl<T> From<Index<T>> for usize {
     fn from(value: Index<T>) -> Self {
         value.index
     }
 }
 
-impl<T> Copy for Index<T> where T: Clone {}
+impl<T> Copy for Index<T> {}
+impl<T> Clone for Index<T> {
+    fn clone(&self) -> Self {
+        Self { index: self.index, _data: PhantomData }
+    }
+}
 
-impl<T> Index<T>
-where
-    T: Clone
-{
+impl<T> Index<T> {
     pub fn new(index: usize) -> Self {
         Self { index: index, _data: PhantomData::default() }
     }
 }
 
-struct ListIndexTransformations<T>(Vec<Index<T>>)
-where
-    T: Clone;
+struct ListIndexTransformations<T>(Vec<Index<T>>);
 
-impl<T> Deref for ListIndexTransformations<T>
-where
-    T: Clone
-{
+impl<T> Deref for ListIndexTransformations<T> {
     type Target = Vec<Index<T>>;
     fn deref(&self) -> &Self::Target {
         &self.0
@@ -77,52 +56,35 @@ where
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct List<T>(Vec<Option<T>>)
-where
-    T: Clone;
+pub struct List<T, I = T>(Vec<Option<T>>, PhantomData<I>);
 
-impl<T> core::ops::Index<Index<T>> for List<T>
-where
-    T: Clone
-{
+impl<T, I> core::ops::Index<Index<I>> for List<T, I> {
     type Output = Option<T>;
-    fn index(&self, index: Index<T>) -> &Self::Output {
+    fn index(&self, index: Index<I>) -> &Self::Output {
         &self.0[index.index]
     }
 }
 
-impl<T> core::ops::IndexMut<Index<T>> for List<T>
-where
-    T: Clone
-{
-    fn index_mut(&mut self, index: Index<T>) -> &mut Self::Output {
+impl<T, I> core::ops::IndexMut<Index<I>> for List<T, I> {
+    fn index_mut(&mut self, index: Index<I>) -> &mut Self::Output {
         &mut self.0[index.index]
     }
 }
 
-impl<T> Default for List<T>
-where
-    T: Clone
-{
+impl<T> Default for List<T> {
     fn default() -> Self {
-        Self(Vec::default())
+        Self(Vec::default(), PhantomData)
     }
 }
 
-impl<T> Deref for List<T>
-where
-    T: Clone
-{
+impl<T> Deref for List<T> {
     type Target = Vec<Option<T>>;
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl<T> DerefMut for List<T>
-where
-    T: Clone
-{
+impl<T> DerefMut for List<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
     }
@@ -144,12 +106,17 @@ where
 //     }
 // }
 
-impl<T> List<T>
-where
-    T: Clone
-{
+impl<T, I> List<T, I> {
     pub fn new(values: Vec<Option<T>>) -> Self {
-        Self(values)
+        Self(values, PhantomData)
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self(Vec::with_capacity(capacity), PhantomData)
+    }
+
+    pub fn with_defaults(len: usize) -> Self {
+        Self((0..len).map(|_| None).collect(), PhantomData)
     }
 
     pub fn take(self) -> Vec<Option<T>> {
